6b25249eb84db71e4e0bbc4029ac71e442f452d8
Petr Kriz <283212@vutbr.cz>
2026-04-08 09:36:04 +0200

    Ahoj

--- searching.py
+++ searching.py
@@ -11,20 +11,55 @@ def read_data(file_name, field):
         field (str): Key to retrieve from the JSON data.
             Must be one of: 'unordered_numbers', 'ordered_numbers' or 'dna_sequence'.
 
-    Returns:
-        list | str | None:
-            - list: If data retrieved by the selected field contains numeric data.
-            - str: If field is 'dna_sequence'.
-            - None: If the field is not supported.
+    # # Returns:
+    #     list | str | None:
+    #          - list: If data retrieved by the selected field contains numeric data.
+    # #        - str: If field is 'dna_sequence'.
+    #          - None: If the field is not supported.
     """
-    # get current working directory path
+
     cwd_path = Path.cwd()
     
     file_path = cwd_path / file_name
 
+    with open(file_path, "r") as file:
+        data = json.load(file)
+
+    if field in data:
+        return data[field]
+
+    return None
+
+
+
+
+def linear_search(prohledavana_sekvence, hledane_cislo):
+
+    slovnik = {}
+    slovnik["count"] = 0
+
+
+    for index, hodnota in enumerate(prohledavana_sekvence):
+        if hodnota == hledane_cislo:
+            slovnik["positions"].append(index)
+            slovnik["count"] += 1
+            continue
+
+    return slovnik
+
+
+
+
+
+
 
 def main():
-    pass
+
+    sequential_data = read_data("sequential.json", "unordered_numbers")
+    print(sequential_data)
+    slovnik = linear_search(prohleda, hleda)
+    print(slovnik)
+
 
 
 if __name__ == "__main__":
